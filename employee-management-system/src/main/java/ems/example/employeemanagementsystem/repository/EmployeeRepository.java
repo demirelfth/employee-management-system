fac7224ae319f6648ef32b1b05ab58c8d0e38f95
Fatih Demirel <demirelfth@users.noreply.github.com>
2022-09-04 21:17:10 +0300

    Initial commit

---
+++ employee-management-system/src/main/java/ems/example/employeemanagementsystem/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package ems.example.employeemanagementsystem.repository;
+
+import ems.example.employeemanagementsystem.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
